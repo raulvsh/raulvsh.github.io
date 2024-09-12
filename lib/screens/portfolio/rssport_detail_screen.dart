2e9e92074f40eb8245ea5c3eb098d89622df3de7
raulvsh <raulvsh@gmail.com>
2024-09-12 18:23:15 +0200

    feat: contenido para hepapp_detail, rssport_detail, solucioneshosteleras_detail

--- lib/screens/portfolio/rssport_detail_screen.dart
+++ lib/screens/portfolio/rssport_detail_screen.dart
@@ -11,12 +11,19 @@ class RSSportDetailScreen extends StatelessWidget {
       backgroundColor: const Color.fromARGB(200, 255, 255, 255),
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(1, 183, 255, 1.0),
-        title: const Text('rssport titulo'),
+        title: const Text('Página web para RS-Sport'),
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
-        child: Text(
-          'descripcion de rssport',
+        child: Text('''
+RS-Sport es una empresa especializada en cronometraje deportivo utilizando tecnología avanzada de chips RFID. Ofrecemos soluciones completas para eventos deportivos, incluyendo cronometraje preciso en puntos intermedios y de meta, generación de clasificaciones en tiempo real, y venta de dorsales personalizados. Nuestro enfoque está en proporcionar un servicio integral y confiable para organizadores y participantes, asegurando que cada detalle del cronometraje se gestione con la máxima precisión.
+
+Nuestra plataforma web, desarrollada en HTML, CSS y JavaScript, permite a los usuarios acceder a la información de eventos, resultados y clasificaciones de manera intuitiva y eficiente. Además, contamos con una aplicación asociada en Python que facilita la gestión de carreras, permitiendo añadir y actualizar eventos mediante archivos JSON, lo que garantiza una integración fluida y una administración sencilla de los datos.
+
+RS-Sport combina tecnología de vanguardia y experiencia en el cronometraje deportivo para ofrecer un servicio destacado y personalizado en cada competición. Explore nuestras capturas de pantalla para ver en detalle cómo nuestra plataforma y servicios pueden mejorar la experiencia de su evento deportivo.
+
+
+Descubra más sobre cómo RS-Sport puede llevar su cronometraje deportivo al siguiente nivel.''',
           style: TextStyle(fontSize: 18.0),
         ),
       ),
--- lib/screens/portfolio/solucioneshosteleras_detail_screen.dart
+++ lib/screens/portfolio/solucioneshosteleras_detail_screen.dart
@@ -10,12 +10,19 @@ class SolucionesHostelerasDetailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(200, 255, 255, 255),
       appBar: AppBar(
-        title: const Text('soluciones titulo'),
+        title: const Text('Gestión de pedidos para solucioneshosteleras.es'),
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
-        child: Text(
-          'descripcion de soluciones',
+        child: Text('''
+Nuestra aplicación de gestión de pedidos para Soluciones Hosteleras está diseñada para facilitar la compra y administración de suministros y equipamiento para negocios de hostelería a través de una conexión directa con la API de Makro. Ofrecemos una amplia gama de productos, incluyendo maquinaria de hostelería, mobiliario, refrigeración, aparatos de cocina, utensilios, menaje, productos desechables y todo lo necesario para equipar cualquier negocio de hostelería de manera profesional y eficiente. Además, hemos ampliado nuestra oferta para incluir purificadores de aire con filtros HEPA, generadores de ozono y otros productos destinados a la protección contra el COVID-19.
+
+La aplicación y su interfaz gráfica están desarrolladas en Python, destacándose por su simplicidad y fiabilidad, en respuesta a la necesidad del cliente de un sistema robusto y fácil de usar. A diferencia de soluciones basadas en web, esta aplicación evita problemas comunes de conectividad, proporcionando una experiencia estable y eficiente en la gestión de pedidos.
+
+Nuestra solución permite a los usuarios realizar pedidos de manera rápida y sin complicaciones, asegurando que sus negocios cuenten siempre con los suministros necesarios para operar con éxito. A continuación, encontrará capturas de pantalla que muestran la funcionalidad y facilidad de uso de nuestra aplicación de gestión de pedidos.
+
+
+Descubra cómo nuestra aplicación puede optimizar la gestión de pedidos para su negocio de hostelería.''',
           style: TextStyle(fontSize: 18.0),
         ),
       ),
